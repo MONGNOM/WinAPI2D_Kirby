7c4223cb829ccba102e4cca0bd09252480426212
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2022-11-10 15:54:39 +0900

    fix : 벽 밀어내기 오류 수정
    부딪히는 방향에 따라 밀어내지 않는 오류 수정

--- WinAPI/CBlock.cpp
+++ WinAPI/CBlock.cpp
@@ -52,31 +52,33 @@ void CBlock::OnCollisionEnter(CCollider* pOther)
 
 		Logger::Debug(L"플레이어가 벽에 부딪혀 밀려납니다");
 	
-		CGameObject* pl = pOther->GetOwner();
-		//pl->SetPos(pl->GetPos().x - 1 ,pl->GetPos().y);
 		//조건 왼쪽이면 충돌시 왼쪽으로 밀기 && 반대방향도 마찬가지
+		CGameObject* pl = pOther->GetOwner();
 		if (GetTilePosX() <= pl->GetPos().x)
-			pl->GetPos().x + 10;
+			pl->SetPos(pl->GetPos().x + 5, pl->GetPos().y);
 		else
-			pl->GetPos().x - 10;
+			pl->SetPos(pl->GetPos().x - 5, pl->GetPos().y);
+
 		 
 	}
 }
 
 void CBlock::OnCollisionStay(CCollider* pOther)
 {
-	/*if (pOther->GetObjName() == L"플레이어")
+	if (pOther->GetObjName() == L"플레이어")
 	{
+
+		Logger::Debug(L"플레이어가 벽에 부딪혀 밀려납니다");
+
+		//조건 왼쪽이면 충돌시 왼쪽으로 밀기 && 반대방향도 마찬가지
 		CGameObject* pl = pOther->GetOwner();
-		pl->SetPos(pl->GetPos().x - 1, pl->GetPos().y);
-		 조건 왼쪽이면 충돌시 왼쪽으로 밀기 && 반대방향도 마찬가지
 		if (GetTilePosX() <= pl->GetPos().x)
-			pl->GetPos().x + 1;
-		else if (GetTilePosX() >= pl->GetPos().x)
-			pl->GetPos().x - 1;
+			pl->SetPos(pl->GetPos().x + 5, pl->GetPos().y);
+		else
+			pl->SetPos(pl->GetPos().x - 5, pl->GetPos().y);
 
-	}*/
 
+	}
 }
 
 void CBlock::OnCollisionExit(CCollider* pOther)
